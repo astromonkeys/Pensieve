d0a1a094ffe896bd86081173a8acc406142a0c57
Noah Zurn <noah.zurn@gmail.com>
2021-12-26 18:38:57 -0600

    added settings - fix night mode colors

--- app/src/main/java/com/example/pensieve/HomeActivity.java
+++ app/src/main/java/com/example/pensieve/HomeActivity.java
@@ -2,9 +2,35 @@ package com.example.pensieve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        // settingsIcon = menu.findItem(R.id.toolbar_settings);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.toolbar_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
